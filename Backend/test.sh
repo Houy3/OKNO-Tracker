9f68e9163751953bc6dfb75d0e4bed12e2b01b3f
fattakhovoe <fattakhovoe@FATTAHOV-OE>
2024-02-21 14:28:15 +0300

    docker compose database(+) + application(-)

--- Backend/test.sh
+++ Backend/test.sh
@@ -1,4 +1,5 @@
 docker build -t tracker .
-docker run --name tracker1 -p  1001:8080 -d tracker
-
+#docker run --name tracker1 -p  1001:8080 -d tracker
+docker network create tracker_network
+docker volume create --name tracker_postgres_volume
 read -n1 -r -p "Press any key to continue..." key
--- demo/test.sh
+++
@@ -1,7 +0,0 @@
-docker build -t tracker .
-
-
-docker run --name tracker1 -p  1001:8080 -d tracker
-
-
-read -n1 -r -p "Press any key to continue..." key
